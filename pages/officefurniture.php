aea2934ba02cbf83f8414c44367706cc3cc9539b
Prasad Wijerathna <139612338+prasadwijerathna@users.noreply.github.com>
2024-12-08 08:11:08 +0000

    code changes

--- pages/officefurniture.php
+++ pages/officefurniture.php
@@ -22,28 +22,28 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="Home Heaven Furniture.html">Home</a>
+          <a class="nav-link active" aria-current="page" href="Home Heaven Furniture.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="Home Heaven Furniture.html">About Us</a>
+          <a class="nav-link" href="Home Heaven Furniture.php">About Us</a>
         </li>
          <li class="nav-item">
-          <a class="nav-link" href="Home Heaven Furniture.html">New Arrivals</a>
+          <a class="nav-link" href="Home Heaven Furniture.php">New Arrivals</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Furniture
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="dining.html">Dining</a></li>
-            <li><a class="dropdown-item" href="officefurniture.html">Office Furniture</a></li>
+            <li><a class="dropdown-item" href="dining.php">Dining</a></li>
+            <li><a class="dropdown-item" href="officefurniture.php">Office Furniture</a></li>
             <li><a class="dropdown-item" href="#">Living Room</a></li>
             <li><a class="dropdown-item" href="#">Bed Room</a></li>
             <li><a class="dropdown-item" href="#">Plastic Products</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="contact.html">Contact Us</a>
+          <a class="nav-link" href="contact.php">Contact Us</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" >Sign In</a>
