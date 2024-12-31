d7331c06832fbf4570421b5f7ab786e45fe13d87
Prasad Wijerathna <139612338+prasadwijerathna@users.noreply.github.com>
2024-12-31 11:40:29 +0000

    updated

--- public/DBconnect.php
+++ public/DBconnect.php
@@ -4,7 +4,7 @@
 define('SERVERNAME', '127.0.0.1:3306');
 define('USERNAME', 'root');
 define('PASSWORD', 'mariadb');
-define('DBNAME', 'Homeheavenfurniture');
+define('DBNAME', 'test');
 try{
 	
 $connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
