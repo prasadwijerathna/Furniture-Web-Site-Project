2d0875c0c827e121fe983a0ba619a056c8455eeb
Prasad Wijerathna <139612338+prasadwijerathna@users.noreply.github.com>
2025-01-03 08:47:47 +0000

    correction

--- pages/payment.php
+++ pages/payment.php
@@ -202,7 +202,7 @@ catch (Exception $e){
 //insert data into students table
 function addData($connect,$fullname,$email,$address,$city,$state,$zip,$nameoncard,$creditcardno,$expmonth,$expyear,$cvv){
     try {
-        $sql = "INSERT INTO contactus VALUES ('$fullname','$email','$address','$city','$state','$zip','$nameoncard','$creditcardno','$expmonth','$expyear','$cvv')";
+        $sql = "INSERT INTO orders VALUES ('$fullname','$email','$address','$city','$state','$zip','$nameoncard','$creditcardno','$expmonth','$expyear','$cvv')";
 
     $result = mysqli_query($connect,$sql);
     if ($result) {
