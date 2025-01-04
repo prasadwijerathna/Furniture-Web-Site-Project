d60a126076647c7f9ae14006fd3a278db1ae4688
Prasad Wijerathna <139612338+prasadwijerathna@users.noreply.github.com>
2025-01-04 18:10:51 +0000

    updated

--- pages/Home Heaven Furniture.php
+++ pages/Home Heaven Furniture.php
@@ -53,7 +53,7 @@
           <a class="nav-link" href="../public/contact.php">Contact Us</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="signin.php">Sign In</a>
+          <a class="nav-link" href="signup.php">Sign Up</a>
         </li>
    </ul>
 
