f174e4864a2ff979f1f3819937079f7324ea0529
Prasad Wijerathna <139612338+prasadwijerathna@users.noreply.github.com>
2025-01-04 19:31:17 +0000

    updated

--- pages/livingroom.php
+++ pages/livingroom.php
@@ -67,12 +67,12 @@
     <div class="card h-100" style="height: 600px">
       <img src="../images/Dining/pearl.png" class="card-img-top" alt="...">
       <div class="card-body">
-        <h4 class="card-title"><b>Regent Bedroom Suite</b></h5>
-        <p class="card-text">Satisfaction of great simplicity is achieved in the development of Regent bedroom suite. Fine architecture of the bed that ensures lasting strength and the wardrobe design will provide sufficient space for your belongings. Moreover, the tall mirror dressing table will improve stylishness of your room thus giving you the best viewing angle..</p>
+        <h4 class="card-title"><b>Sentinal Sofa</b></h5>
+        <p class="card-text">Aesthetically pleasing creation of refined excellence. Especially formed distinctive arm pattern along with the uniquely tufted vertical lines proudly stands out symbolizing finest craftsmanship. The built to last frame with carved bun footing further adds charisma to your living room.</p>
 		<p><b><i>Warranty</i></b>
       	<ul>
-      		<li>15 Year Warranty for bed</li>
-      		<li>3 Year Warranty for Wardrobe,Dressing Table,Dressing Stool & Bedside Rack.</li>
+      		<li>10 Years for Wooden Structure.</li>
+      		<li>3 Years for Fabric Upholstery and Cushions.</li>
             <li>Warranty Covers Only Manufacturing Defects.</li>
 		</ul>
 		<a href="signin.php" class="btn btn-primary">Buy Now</a>
@@ -86,15 +86,13 @@
     <div class="card h-100">
       <img src="../images/Dining/willow.png" class="card-img-top" alt="...">
       <div class="card-body">
-        <h4 class="card-title"><b>Regent Bedside Rack – KCRE 001</b></h5>
-        <p class="card-text">Handy stand made of easy to clean scratch resistant materials to hold a night lamp just beside your bed. Additional shelf will hold your favourite magazine in a conveniently accessible height.</p>
+        <h4 class="card-title"><b>Baywood Sofa</b></h5>
+        <p class="card-text">Complement your living space with a classical touch of a brilliantly crafted wooden sofa. The perfected finish of the wooden ladder pattern frame with appealingly tailored fabric upholstery and the similar type center table will provide a distinctive appearance.</p>
         <p><b><i>Warranty</i></b>
       	<ul>
       		<li>3 Year comprehensive warranty</li>
       		<li>Warranty Covers Only Manufacturing Defects.</li>
-		</ul></p><b><i>Dimension</i></b>
-		<ul><li>Length – 40cm | Width – 32cm | Height – 50cm</li>
-		</ul>
+		</ul></p>
 		<a href="signin.php" class="btn btn-primary">Buy Now</a>
     <br>
       </div>
@@ -107,13 +105,9 @@
     <div class="card h-100">
       <img src="../images/Dining/nevada.png" class="card-img-top" alt="...">
       <div class="card-body">
-        <h4 class="card-title"><b>Ornate 2 Door Wardrobe (KWO 002)</b></h5>
-        <p class="card-text">Impress your room’s beauty with a dark walnut colour shade. Sturdy development with scratch resistant melamine faced surfaces will let you experience a superior finish. Chrome touched handles stands out presenting a contrastive look.</p>
-        <p><b><i>Warranty</i></b>
-      	<ul>
-      		<li>2 year comprehensive Warranty</li>
-      		<li>Warranty Covers Only Manufacturing Defects.</li>
-		</ul><br><br>
+        <h4 class="card-title"><b>Wall Shelf</b></h5>
+        <p class="card-text">Add colour to your wall with splendid formation of three stepped collectible holder. Precision workmanship is highlighted and the use of scratch resistant materials provides a lasting effect.</p>
+        <br><br>
 		<a href="signin.php" class="btn btn-primary">Buy Now</a>
       </div>
       <div class="card-footer">
@@ -128,8 +122,8 @@
     <div class="card h-100" style="height: 600px">
       <img src="../images/Dining/cbint.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h4 class="card-title"><b>Spring Mattress</b></h5>
-        <p class="card-text">Give yourself the relaxation that you deserve the most with Classic 7 mattress. Careful selection of suitable springs will ensure comforting support in every posture. Admirable tailoring of the soothing upholstery will usher you to a blissful night’s sleep.</p>
+        <h4 class="card-title"><b>Wall Unit</b></h5>
+        <p class="card-text">An ingenious production that advances your living room’s interior to the next level. The massive architecture of the wall unit allows you to effortlessly house your big screen television whilst neatly organizing other entertainment gadgets in place. Also, equips with ornament racks, cupboards and a cabinet with tempered glass doors for efficient storage management.</p>
         <p><b><i>Warranty</i></b>
         <ul>
           <li>15 Year Warranty for spring structure</li>
